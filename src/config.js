4e00b81277694927198e214c3d57f57ddcedee5d
kirstenflo <floyd.kirsten@yahoo.com>
2019-07-31 11:13:47 -0400

    update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,5 @@
 module.exports = {
+    url: "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=TRHC&apikey=KVIFMK752EG4IWVY",
     appID: "e891f445c1684e29be5b0d23899c1dec",
     cityID: "4588165"
 }
