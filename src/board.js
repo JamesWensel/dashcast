7ef39f09d392194f1ac1f21e80268fe1cd9a611b
kirstenflo <floyd.kirsten@yahoo.com>
2019-07-31 12:57:25 -0400

    Update board.js

--- src/board.js
+++ src/board.js
@@ -1,11 +1,30 @@
 import React, { useState, useEffect } from "react";
 import WeatherWidget from "./weather/WeatherWidget";
 import StockWidget from "./stock/StockWidget";
+import Clock from "./clock/Clock";
 import "./dash.css";
 
 function Board() {
+
+    useEffect(() => {
+        async function changeImage() {
+            const image = await fetch('https://source.unsplash.com/collection/8272744/1800x1200')
+            const url = image.url;
+            console.log(image.url);
+            document.body.style.backgroundImage = "url(" + image.url + ")";
+        }
+        changeImage();
+
+        const interval = setInterval(() => {
+            changeImage();
+          }, 10000);
+          return () => clearInterval(interval);
+
+    }, []);
+    
+
     return (
-        <div id="board">
+        <div id="board" >
             <div id="widget"> 
                  <WeatherWidget /> 
             </div> 
