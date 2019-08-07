873e402346c0f42a1e877cd0924d20f8aac1ca87
JamesWensel <jdwensel@ksu.edu>
2019-08-07 13:01:42 -0400

    Added Difference in Stock Price over Day

--- src/stock/Stock.js
+++ src/stock/Stock.js
@@ -5,10 +5,17 @@ const config = require("../config.js");
 const url = config.url;
 
 function Stock() {
-  let { close } = useStockAPI(url);
-  let formattedData = parseFloat(close).toFixed(2);
+  let { open, close } = useStockAPI(url);
+  let formattedClose = parseFloat(close).toFixed(2);
+  let formattedOpen = parseFloat(open).toFixed(2);
+  let difference = (formattedClose - formattedOpen).toFixed(2);
+  let change = difference >= 0 ? "+$" + difference : "$" + difference;
 
-  return <h1 id="stockPrice">TRHC Stock Price: ${formattedData}</h1>;
+  return (
+    <h1 id="stockPrice">
+      TRHC Stock Price: ${formattedClose} {change}
+    </h1>
+  );
 }
 
 export default Stock;
